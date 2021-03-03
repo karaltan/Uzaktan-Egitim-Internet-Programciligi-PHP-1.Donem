a69f7014f02631964d7a63e82a56a681c0d0a2da
Altan KARAALP <karaltan@gmail.com>
2021-03-04 01:15:03 +0300

    Add files via upload

---
+++ 9-22.01.2021/FORMLAR/GET-ORNEKLER/yaz.php
@@ -0,0 +1,20 @@
+<!--yaz.php-->
+<html>
+<head>
+  <meta charset="UTF-8">
+</head>
+<body>
+
+<?php
+$gelen_isim=$_GET["adi"];
+$gelen_soyisim=$_GET["soyadi"];
+
+echo "<b>Adınız:</b> ",$gelen_isim,"<br>";
+echo "<b>Soyadınız:</b> ",$gelen_soyisim,"<br>";
+
+?>
+
+</body>
+
+
+</html>
